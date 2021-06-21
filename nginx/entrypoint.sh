1a011ae598c603acf5f9064b4735706c3da2a9d8
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-06-21 13:49:04 -0700

    nginx.conf template update for logs

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,5 +2,6 @@
 set -eu
 
 envsubst '${APP_NAME}' < /etc/nginx/conf.d/xtages.conf.template > /etc/nginx/conf.d/xtages.conf
+envsubst '${ORGANIZATION_HASH} ${APP_NAME} ${APP_ENV}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
