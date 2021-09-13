7ddb61de3604cfe0e2fa40822423eb84bc7cdfd5
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-09-13 09:35:18 -0700

    adding account id as part of the init (#8)

--- cd/nodejs-15.13.0/init.sh
+++ cd/nodejs-15.13.0/init.sh
@@ -1,2 +1,3 @@
 nohup /usr/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2 > /dev/null 2>&1 &
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 605769209612.dkr.ecr.us-east-1.amazonaws.com > /dev/null 2>&1
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r ".Account")
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}".dkr.ecr.us-east-1.amazonaws.com > /dev/null 2>&1
